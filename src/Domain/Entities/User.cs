1021ad8d2cd7df8c7eebd5cd6ee333d04f97d7b5
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-22 20:42:20 -0300

    feat: add user entity

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,18 +1,17 @@
-namespace UserCrud.Domain.Entities
+namespace UserCrud.Domain.Entities;
+
+public class User : Entity
 {
-    public class User : Entity
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public ICollection<Phone> Phones { get; set; }
+
+    public User(string name, string email, string password)
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-
-        public ICollection<Phone> Phones { get; set; }
-
-        public User(string name, string email, string password)
-        {
-            Name = name;
-            Email = email;
-            Password = password;
-        }
+        Name = name;
+        Email = email;
+        Password = password;
     }
 }
