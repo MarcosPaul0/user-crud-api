6beb7a761be89a2911c97279f8775d6715899be3
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-24 14:33:14 -0300

    feat: add static class for common exception messages

---
+++ src/Application/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+namespace UserCrud.Application.Exceptions;
+
+public static class ExceptionMessages
+{
+    public const string USER_NOT_FOUND = "User not found!";
+    public const string USER_ALREADY_EXISTS = "User already exists!";
+}
