dcb2c779286e786d4fbf133d187feb98e6a80565
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-26 20:13:18 -0300

    refactor: update `UserConfiguration` to use modern C# syntax and configure entity properties with column mappings

--- src/Infrastructure/EntitiesConfiguration/UserConfiguration.cs
+++ src/Infrastructure/EntitiesConfiguration/UserConfiguration.cs
@@ -2,27 +2,26 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using UserCrud.Domain.Entities;
 
-namespace UserCrud.Infrastructure.EntitiesConfiguration
+namespace UserCrud.Infrastructure.EntitiesConfiguration;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
-    public class UserConfiguration : IEntityTypeConfiguration<User>
+    public void Configure(EntityTypeBuilder<User> builder)
     {
-        public void Configure(EntityTypeBuilder<User> builder)
-        {
-            builder.HasKey(user => user.Id);
+        builder.HasKey(user => user.Id);
+        
+        builder.ToTable("user");
 
-            builder.Property(user => user.Id)
-                .ValueGeneratedOnAdd();
-            builder.Property(user => user.CreatedAt)
-                .ValueGeneratedOnAdd();
-            builder.Property(user => user.UpdatedAt).ValueGeneratedOnAdd();
-            builder.Property(user => user.Name).HasMaxLength(50).IsRequired();
-            builder.Property(user => user.Email).HasMaxLength(255).IsRequired();
-            builder.Property(user => user.Password).HasMaxLength(50).IsRequired();
+        builder.Property(user => user.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        builder.Property(user => user.Name).HasColumnName("name").HasMaxLength(50).IsRequired();
+        builder.Property(user => user.Email).HasColumnName("email").HasMaxLength(255).IsRequired();
+        builder.Property(user => user.Password).HasColumnName("password").HasMaxLength(50).IsRequired();
+        builder.Property(user => user.CreatedAt).HasColumnName("created_at").ValueGeneratedOnAdd();
+        builder.Property(user => user.UpdatedAt).HasColumnName("updated_at").IsRequired(false).ValueGeneratedOnUpdate();
 
-            builder.HasMany(entity => entity.Phones)
-                .WithOne(entity => entity.User)
-                .HasForeignKey(entity => entity.UserId)
-                .IsRequired();
-        }
+        builder.HasMany(entity => entity.Phones)
+            .WithOne(entity => entity.User)
+            .HasForeignKey(entity => entity.UserId)
+            .IsRequired();
     }
 }
