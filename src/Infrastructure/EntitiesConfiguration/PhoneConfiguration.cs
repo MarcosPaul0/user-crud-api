dc6bf1fc92eaf23b57ddfc35c0a33b6d54396bc8
MarcosPaul0 <marcosphip7@gmail.com>
2026-01-26 20:12:56 -0300

    refactor: update `PhoneConfiguration` to use modern C# syntax and configure entity properties with column mappings

--- src/Infrastructure/EntitiesConfiguration/PhoneConfiguration.cs
+++ src/Infrastructure/EntitiesConfiguration/PhoneConfiguration.cs
@@ -2,22 +2,24 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using UserCrud.Domain.Entities;
 
-namespace UserCrud.Infrastructure.EntitiesConfiguration
+namespace UserCrud.Infrastructure.EntitiesConfiguration;
+
+public class PhoneConfiguration : IEntityTypeConfiguration<Phone>
 {
-    public class PhoneConfiguration : IEntityTypeConfiguration<Phone>
+    public void Configure(EntityTypeBuilder<Phone> builder)
     {
-        public void Configure(EntityTypeBuilder<Phone> builder)
-        {
-            builder.HasKey(phone => phone.Id);
+        builder.HasKey(phone => phone.Id);
+        
+        builder.ToTable("phone");
 
-            builder.Property(user => user.CreatedAt).ValueGeneratedOnAdd();
-            builder.Property(user => user.UpdatedAt).ValueGeneratedOnAdd();
-            builder.Property(phone => phone.Id).ValueGeneratedOnAdd();
-            builder.Property(phone => phone.PhoneNumber).HasMaxLength(13).IsRequired();
+        builder.Property(phone => phone.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        builder.Property(phone => phone.PhoneNumber).HasColumnName("phone_number").HasMaxLength(13).IsRequired();
+        builder.Property(phone => phone.UserId).HasColumnName("user_id").IsRequired();
+        builder.Property(user => user.CreatedAt).HasColumnName("created_at").IsRequired().ValueGeneratedOnAdd();
+        builder.Property(user => user.UpdatedAt).HasColumnName("updated_at").IsRequired(false).ValueGeneratedOnUpdate();
 
-            builder.HasOne(entity => entity.User)
-               .WithMany(entity => entity.Phones)
-               .HasForeignKey(entity => entity.UserId);
-        }
+        builder.HasOne(entity => entity.User)
+           .WithMany(entity => entity.Phones)
+           .HasForeignKey(entity => entity.UserId);
     }
 }
