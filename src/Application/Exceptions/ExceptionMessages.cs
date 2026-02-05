6634a9ce9890417ae73a3c72a1882a06e0cfae6c
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-04 22:23:49 -0300

    refactor: remove `ExceptionHandlingMiddleware` and improve exception messages in `ExceptionMessages` class

--- src/Application/Exceptions/ExceptionMessages.cs
+++ src/Application/Exceptions/ExceptionMessages.cs
@@ -2,6 +2,14 @@ namespace UserCrud.Application.Exceptions;
 
 public static class ExceptionMessages
 {
+    public const string LOGIN_FAILED = "Email or password is incorrect!";
+    
     public const string USER_NOT_FOUND = "User not found!";
     public const string USER_ALREADY_EXISTS = "User already exists!";
+    
+    public const string PRODUCT_CATEGORY_NOT_FOUND = "Product category not found!";
+    public const string PRODUCT_CATEGORY_ALREADY_EXISTS = "Product category already exists!";
+    
+    public const string PRODUCT_NOT_FOUND = "Product not found!";
+    public const string PRODUCT_ALREADY_EXISTS = "Product already exists!";
 }
