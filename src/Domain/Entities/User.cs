cdda3a9cff7fafc9e5fe697ebb38a0d95f801efe
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-04 22:24:51 -0300

    feat: add `Role` and `CreatedAt` properties to `User` entity, update constructor to initialize new fields

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,3 +1,5 @@
+using UserCrud.Domain.Enums;
+
 namespace UserCrud.Domain.Entities;
 
 public class User : Entity
@@ -5,13 +7,15 @@ public class User : Entity
     public string Name { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
+    
+    public UserRole Role { get; set; }
 
-    public ICollection<Phone> Phones { get; set; }
-
-    public User(string name, string email, string password)
+    public User(string name, string email, string password, UserRole role, DateTime createdAt) 
     {
         Name = name;
         Email = email;
         Password = password;
+        Role = role;
+        CreatedAt = createdAt;
     }
 }
