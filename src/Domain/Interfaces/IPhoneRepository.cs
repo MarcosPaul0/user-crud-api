e1fb7a47cbbcb6f51585fb6232a50b1279ec9599
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-04 22:24:02 -0300

    refactor: simplify `PhoneRepository` by inheriting from `BaseRepository`, remove redundant methods, and update `IPhoneRepository` interface accordingly

--- src/Domain/Interfaces/IPhoneRepository.cs
+++ src/Domain/Interfaces/IPhoneRepository.cs
@@ -2,12 +2,8 @@ using UserCrud.Domain.Entities;
 
 namespace UserCrud.Domain.Interfaces;
 
-public interface IPhoneRepository
+public interface IPhoneRepository : IBaseRepository<Phone>
 {
     Task<IEnumerable<Phone>> FindAllAsync(CancellationToken cancellationToken = default);
-    Task<Phone?> FindByIdAsync(string id, CancellationToken cancellationToken = default);
     Task<IEnumerable<Phone>> FindByUserIdAsync(string userId, CancellationToken cancellationToken = default);
-    Task<Phone> CreateAsync(Phone phone, CancellationToken cancellationToken = default);
-    Task<Phone> UpdateAsync(Phone phone);
-    Task<Phone> DeleteAsync(Phone phone);
 }
--- src/Infrastructure/Repositories/PhoneRepository.cs
+++ src/Infrastructure/Repositories/PhoneRepository.cs
@@ -5,43 +5,19 @@ using UserCrud.Infrastructure.Context;
 
 namespace UserCrud.Infrastructure.Repositories;
 
-public class PhoneRepository(ApplicationDbContext context) : IPhoneRepository
+public class PhoneRepository(ApplicationDbContext context) : BaseRepository<Phone>(context), IPhoneRepository
 {
-    public async Task<Phone> CreateAsync(Phone phone, CancellationToken cancellationToken = default)
-    {
-        await context.AddAsync(phone, cancellationToken);
-        
-        return phone;
-    }
-
-    public async Task<Phone> DeleteAsync(Phone phone)
-    {
-        context.Remove(phone);
-        
-        return await Task.FromResult(phone);
-    }
+    private readonly ApplicationDbContext _context = context;
 
     public async Task<IEnumerable<Phone>> FindAllAsync(CancellationToken cancellationToken = default)
     {
-        return await context.Phone.AsNoTracking().ToListAsync(cancellationToken);
-    }
-
-    public async Task<Phone?> FindByIdAsync(string id, CancellationToken cancellationToken = default)
-    {
-        return await context.Phone.FindAsync(id, cancellationToken);
+        return await _context.Phone.AsNoTracking().ToListAsync(cancellationToken);
     }
 
     public async Task<IEnumerable<Phone>> FindByUserIdAsync(string userId, CancellationToken cancellationToken = default)
     {
-        return await context.Phone
+        return await _context.Phone
             .Where(phone => phone.UserId == Guid.Parse(userId))
             .ToListAsync(cancellationToken);
-    }
-
-    public async Task<Phone> UpdateAsync(Phone phone)
-    {
-        context.Update(phone);
-        
-        return await Task.FromResult(phone);
     }
 }
