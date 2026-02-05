3ad23f72fc5aa1437f6f6e3674a3ae60cdda763e
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-04 22:23:43 -0300

    refactor: make `UpdatedAt` property setter public in `Entity` class

--- src/Domain/Entities/Entity.cs
+++ src/Domain/Entities/Entity.cs
@@ -4,5 +4,5 @@ public abstract class Entity
 {
     public Guid Id { get; protected set; }
     public DateTime CreatedAt { get; protected set; }
-    public DateTime? UpdatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; set; }
 }
