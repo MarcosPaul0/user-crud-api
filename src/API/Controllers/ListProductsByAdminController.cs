c80fed4f31eb252229e4934ead28ed90611c651e
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-05 22:11:25 -0300

    feat: add `ListProductsByAdminController` to handle product listing requests by admin

---
+++ src/API/Controllers/ListProductsByAdminController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using UserCrud.Application.Dtos;
+using UserCrud.Application.UseCases.ListProductsByAdmin;
+
+namespace UserCrud.API.Controllers;
+
+[ApiController]
+[Route("api/product/by-admin")]
+public class ListProductsByAdminController(IListProductsByAdminUseCase listProductsByAdminUseCase) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult> HandleAsync(
+        [FromBody] ListProductByAdminDto listProductByAdminDto,
+        CancellationToken cancellationToken)
+    {
+        var result = await listProductsByAdminUseCase.ExecuteAsync(listProductByAdminDto, cancellationToken);
+
+        return Ok();
+    }
+}
