aaf1c4a220ceaf887abe503ddbafede00e00b20b
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-08 09:29:15 -0300

    feat: add filtering, pagination, and counting support to UserRepository

--- src/Domain/Interfaces/IUserRepository.cs
+++ src/Domain/Interfaces/IUserRepository.cs
@@ -4,6 +4,11 @@ namespace UserCrud.Domain.Interfaces;
 
 public interface IUserRepository : IBaseRepository<User>
 {
-    Task<IEnumerable<User>> FindAllAsync(CancellationToken cancellationToken= default);
+    Task<IEnumerable<User>> FindAllAsync(
+        User filter, 
+        int page, 
+        int itemsPerPage, 
+        CancellationToken cancellationToken= default);
+    Task<int> CountAsync(User filter, CancellationToken cancellationToken= default);
     Task<User?> FindByEmailAsync(string email, CancellationToken cancellationToken= default);
 }
--- src/Infrastructure/Repositories/UserRepository.cs
+++ src/Infrastructure/Repositories/UserRepository.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using UserCrud.Domain.Entities;
 using UserCrud.Domain.Interfaces;
 using UserCrud.Infrastructure.Context;
+using UserCrud.Infrastructure.Repositories.FilterBuilders;
 
 namespace UserCrud.Infrastructure.Repositories;
 
@@ -9,10 +10,35 @@ public class UserRepository(ApplicationDbContext context) : BaseRepository<User>
 {
     private readonly ApplicationDbContext _context = context;
 
-    public async Task<IEnumerable<User>> FindAllAsync(CancellationToken cancellationToken = default)
+    public async Task<IEnumerable<User>> FindAllAsync(
+        User filter, 
+        int page, 
+        int itemsPerPage, 
+        CancellationToken cancellationToken = default)
     {
-        return await _context.User.AsNoTracking().ToListAsync(cancellationToken);
+        var query = _context.User.AsNoTracking();
+
+        query = new UserFilterBuilder(query)
+            .FilterByName(filter.Name)
+            .FilterByRole(filter.Role)
+            .ApplyPagination(page, itemsPerPage)
+            .Build();
+        
+        return await query.ToListAsync(cancellationToken);
     }
+
+    public async Task<int> CountAsync(User filter, CancellationToken cancellationToken = default)
+    {
+        var query = _context.User.AsNoTracking();
+
+        query = new UserFilterBuilder(query)
+            .FilterByName(filter.Name)
+            .FilterByRole(filter.Role)
+            .Build();
+        
+        return await query.CountAsync(cancellationToken);
+    }
+
 
     public async Task<User?> FindByEmailAsync(string email, CancellationToken cancellationToken = default)
     {
@@ -21,7 +47,6 @@ public class UserRepository(ApplicationDbContext context) : BaseRepository<User>
 
     public async Task<User?> FindWithPonesByIdAsync(Guid id, CancellationToken cancellationToken = default)
     {
-        return await _context.User
-            .FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
+        return await _context.User.FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
     }
 }
