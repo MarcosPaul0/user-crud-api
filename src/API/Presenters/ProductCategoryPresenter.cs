fe9cd005f91d2f46219a26d04d0c4e88315c7f8f
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-08 09:30:37 -0300

    feat: add `ProductCategoryPresenter` with conversion methods for response DTOs

---
+++ src/API/Presenters/ProductCategoryPresenter.cs
@@ -0,0 +1,33 @@
+using UserCrud.API.Dtos;
+using UserCrud.Domain.Entities;
+
+namespace UserCrud.API.Presenters;
+
+public static class ProductCategoryPresenter
+{
+    public static ProductCategoryResponseDto ToHttp(ProductCategory productCategory)
+    {
+        
+        return new ProductCategoryResponseDto()
+        {
+            Id = productCategory.Id,
+            Category = productCategory.Category,
+        };
+    }
+    
+    public static PaginationResponseDto<ProductCategoryResponseDto> ToHttp(IEnumerable<ProductCategory> products, int count)
+    {
+        var productCategoriesResponse = products.Select(ToHttp);
+        
+        return new PaginationResponseDto<ProductCategoryResponseDto>()
+        {
+            HasNext = false,
+            HasPrevious = false,
+            Items = productCategoriesResponse,
+            ItemsPerPage = count,
+            TotalItems = count,
+            Page = 1,
+            TotalPages = 1,
+        };
+    }
+}
