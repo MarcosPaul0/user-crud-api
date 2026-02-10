0b30ebff57aed0e3e9fd54ca42317f46a5d003ea
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-09 22:23:51 -0300

    feat: delete product images from storage during product deletion

--- src/Application/UseCases/DeleteProduct/DeleteProductUseCase.cs
+++ src/Application/UseCases/DeleteProduct/DeleteProductUseCase.cs
@@ -1,10 +1,13 @@
 using UserCrud.Application.Exceptions;
+using UserCrud.Application.Interfaces;
 using UserCrud.Domain.Interfaces;
 
 namespace UserCrud.Application.UseCases.DeleteProduct;
 
 public class DeleteProductUseCase(
-    IProductRepository productRepository, 
+    IObjectStorageService objectStorageService,
+    IProductRepository productRepository,
+    IProductImageRepository productImageRepository,
     IUnitOfWork unitOfWork) : IDeleteProductUseCase
 {
     public async Task ExecuteAsync(Guid productId, CancellationToken cancellationToken)
@@ -16,6 +19,13 @@ public class DeleteProductUseCase(
             throw new NotFoundException(ExceptionMessages.PRODUCT_NOT_FOUND);
         }
         
+        var productImages = await productImageRepository.FindAllByProductIdAsync(productId, cancellationToken);
+
+        foreach (var productImage in productImages)
+        {
+            await objectStorageService.DeleteAsync(productImage.ImageUrl, cancellationToken);
+        }
+
         await productRepository.DeleteAsync(product, cancellationToken);
         
         await unitOfWork.SaveChangesAsync();
