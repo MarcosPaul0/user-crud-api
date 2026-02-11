559877333d7a39d1e70b0852d71ae9c57d01ff77
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-10 21:46:01 -0300

    feat: add migrations for `Product`, `ProductCategory`, and `ProductImage` tables

---
+++ src/Infrastructure/Migrations/20260208125652_AddProductTables.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserCrud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "product_category",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    category = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product_category", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    description = table.Column<string>(type: "character varying(1200)", maxLength: 1200, nullable: false),
+                    price_in_cents = table.Column<int>(type: "integer", nullable: false),
+                    ProductionTimeInDays = table.Column<int>(type: "integer", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    stock_quantity = table.Column<int>(type: "integer", nullable: false),
+                    product_category_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_product_product_category_product_category_id",
+                        column: x => x.product_category_id,
+                        principalTable: "product_category",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product_image",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    image_url = table.Column<string>(type: "character varying(500)", maxLength: 500, nullable: false),
+                    display_order = table.Column<byte>(type: "smallint", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product_image", x => x.id);
+                    table.CheckConstraint("CK_ProductImage_DisplayOrder", "display_order BETWEEN 1 AND 5");
+                    table.ForeignKey(
+                        name: "FK_product_image_product_product_id",
+                        column: x => x.product_id,
+                        principalTable: "product",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_product_category_id",
+                table: "product",
+                column: "product_category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_image_product_id_display_order",
+                table: "product_image",
+                columns: new[] { "product_id", "display_order" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product_image");
+
+            migrationBuilder.DropTable(
+                name: "product");
+
+            migrationBuilder.DropTable(
+                name: "product_category");
+        }
+    }
+}
