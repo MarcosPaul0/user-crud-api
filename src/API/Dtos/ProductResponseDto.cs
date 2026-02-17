d47e421237bc5edb8e39dc71d26a88c70557e95e
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-17 20:55:34 -0300

    feat: update `ProductResponseDto` to include `DiscountPercentage` and rename `ProductionTimeInDays` to `ProductionTimeInMinutes`

--- src/API/Dtos/ProductResponseDto.cs
+++ src/API/Dtos/ProductResponseDto.cs
@@ -6,7 +6,8 @@ public record ProductResponseDto
     public required string Name { get; init; }
     public required string Description { get; init; }
     public required int PriceInCents { get; init; }
-    public required int ProductionTimeInDays { get; init; }
+    public required int ProductionTimeInMinutes { get; init; }
+    public required byte DiscountPercentage { get; init; }
     public required Guid ProductCategoryId { get; init; }
     public required string Category { get; init; }
 }
