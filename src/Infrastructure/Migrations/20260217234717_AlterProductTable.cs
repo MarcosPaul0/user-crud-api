466ca2aae21d1c2d061bedfcd18633793cc4e9ea
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-17 20:55:52 -0300

    feat: add migration for `Product` table to include `DiscountPercentage` and rename `ProductionTimeInDays` to `ProductionTimeInMinutes`

---
+++ src/Infrastructure/Migrations/20260217234717_AlterProductTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserCrud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductionTimeInDays",
+                table: "product",
+                newName: "production_time_in_minutes");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "discount_percentage",
+                table: "product",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "discount_percentage",
+                table: "product");
+
+            migrationBuilder.RenameColumn(
+                name: "production_time_in_minutes",
+                table: "product",
+                newName: "ProductionTimeInDays");
+        }
+    }
+}
