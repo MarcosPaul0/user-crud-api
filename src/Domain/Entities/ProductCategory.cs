d0d0dce096d9dd2f6d117e47f9ad03fc3b0e1a42
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-20 23:42:24 -0300

    feat: add `ProductCount` property to `ProductCategory` with JSON ignore and database non-mapping attributes

--- src/Domain/Entities/ProductCategory.cs
+++ src/Domain/Entities/ProductCategory.cs
@@ -1,12 +1,28 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
 namespace UserCrud.Domain.Entities;
 
 public class ProductCategory : Entity
 {
     public string Category { get; set; }
+    
+    [NotMapped]
+    [JsonIgnore]
+    public int ProductCount { get; set; }
 
     public ProductCategory(string category, DateTime createdAt)
     {
         Category = category;
         CreatedAt = createdAt;
+    }
+    
+    public ProductCategory(Guid id, string category, int productCount, DateTime createdAt, DateTime? updatedAt)
+    {
+        Id = id;
+        Category = category;
+        ProductCount = productCount;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
     }
 }
