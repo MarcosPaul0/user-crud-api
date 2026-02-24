e3dc2c78739e666ec6eea91b782b81fb903c9e6e
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-23 23:19:37 -0300

    refactor: drop composite index on `product_image` table and create single-column index on `product_id`

---
+++ src/Infrastructure/Migrations/20260222223632_DropDisplayOrderProductImageTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserCrud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropDisplayOrderProductImageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_product_image_product_id_display_order",
+                table: "product_image");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_image_product_id",
+                table: "product_image",
+                column: "product_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_product_image_product_id",
+                table: "product_image");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_image_product_id_display_order",
+                table: "product_image",
+                columns: new[] { "product_id", "display_order" },
+                unique: true);
+        }
+    }
+}
