00129b2aea315a15dd8f9aaf6538e12dfeb81196
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-23 23:20:16 -0300

    feat: add presenter and DTO for transforming `ProductImage` into HTTP response data

---
+++ src/API/Dtos/ProductImageResponseDto.cs
@@ -0,0 +1,10 @@
+namespace UserCrud.API.Dtos;
+
+public record ProductImageResponseDto
+{
+    public required Guid Id { get; init; }
+    public required string ImageUrl { get; init; }
+    public required byte DisplayOrder { get; init; }
+    public required DateTime CreatedAt { get; init; }
+    public required DateTime? UpdatedAt { get; init; }
+}
---
+++ src/API/Presenters/ProductImagePresenter.cs
@@ -0,0 +1,24 @@
+using UserCrud.API.Dtos;
+using UserCrud.Domain.Entities;
+
+namespace UserCrud.API.Presenters;
+
+public static class ProductImagePresenter
+{
+    public static ProductImageResponseDto ToHttp(ProductImage productImage)
+    {
+        return new ProductImageResponseDto()
+        {
+            Id = productImage.Id,
+            ImageUrl = productImage.ImageUrl,
+            DisplayOrder = productImage.DisplayOrder,
+            CreatedAt = productImage.CreatedAt,
+            UpdatedAt = productImage.UpdatedAt,
+        };
+    }
+    
+    public static List<ProductImageResponseDto> ToHttp(List<ProductImage> productImages)
+    {
+        return productImages.Select(ToHttp).ToList();
+    }
+}
