e50b1c6d3a0ee1246801977663fb163f6447c694
MarcosPaul0 <marcosphip7@gmail.com>
2026-02-28 10:59:14 -0300

    feat: add `print_description` column to `Product` entity with migration

---
+++ src/Infrastructure/Migrations/20260225012921_ProductPrintDescriptionColumn.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserCrud.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductPrintDescriptionColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "print_description",
+                table: "product",
+                type: "character varying(600)",
+                maxLength: 600,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "print_description",
+                table: "product");
+        }
+    }
+}
