7b9931e1c550ee8f3d6dfadb932ad24c688d3159
MarcosPaul0 <marcosphip7@gmail.com>
2026-03-17 22:26:47 -0300

    chore: refactor `UnitOfWork` to use lazy-loaded repositories and implement `IDisposable`

--- src/Infrastructure/Repositories/UnitOfWork.cs
+++ src/Infrastructure/Repositories/UnitOfWork.cs
@@ -1,12 +1,45 @@
+using Microsoft.Extensions.DependencyInjection;
 using UserCrud.Domain.Interfaces;
 using UserCrud.Infrastructure.Context;
 
 namespace UserCrud.Infrastructure.Repositories;
 
-public class UnitOfWork(ApplicationDbContext context) : IUnitOfWork
+public sealed class UnitOfWork : IUnitOfWork
 {
+    private readonly ApplicationDbContext _context;
+
+    private readonly Lazy<IUserRepository> _user;
+    private readonly Lazy<IProductRepository> _product;
+    private readonly Lazy<IProductCategoryRepository> _productCategory;
+    private readonly Lazy<IProductImageRepository> _productImage;
+
+    public UnitOfWork(ApplicationDbContext context, IServiceProvider serviceProvider)
+    {
+        _context = context;
+        
+        _user = new Lazy<IUserRepository>(
+            serviceProvider.GetRequiredService<IUserRepository>);
+        _product = new Lazy<IProductRepository>(
+            serviceProvider.GetRequiredService<IProductRepository>);
+        _productCategory = new Lazy<IProductCategoryRepository>(
+            serviceProvider.GetRequiredService<IProductCategoryRepository>);
+        _productImage = new Lazy<IProductImageRepository>(
+            serviceProvider.GetRequiredService<IProductImageRepository>);
+    }
+    
+    public IUserRepository User => _user.Value;
+    public IProductRepository Product => _product.Value;
+    public IProductCategoryRepository ProductCategory => _productCategory.Value;
+    public IProductImageRepository ProductImage => _productImage.Value;
+    
     public async Task SaveChangesAsync()
     {
-        await context.SaveChangesAsync();
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
