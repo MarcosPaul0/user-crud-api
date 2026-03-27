821e09ace63bd4136ff0714f6dd1dee755962a34
MarcosPaul0 <marcosphip7@gmail.com>
2026-03-26 22:29:25 -0300

    feat: add `IsActive` property to `ProductCategory` and update configuration for default value and required constraint

--- src/Domain/Entities/ProductCategory.cs
+++ src/Domain/Entities/ProductCategory.cs
@@ -1,11 +1,13 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace UserCrud.Domain.Entities;
+namespace AutoriaStore.Domain.Entities;
 
 public class ProductCategory : Entity
 {
     public string Category { get; set; }
+    
+    public bool IsActive { get; set; }
     
     [NotMapped]
     [JsonIgnore]
