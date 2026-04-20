c82488161d18604824a065a03766cbeefaf1f963
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-20 16:01:12 -0300

    feat: add order and idempotency to unit of work

--- src/Domain/Interfaces/IUnitOfWork.cs
+++ src/Domain/Interfaces/IUnitOfWork.cs
@@ -6,5 +6,7 @@ public interface IUnitOfWork : IDisposable
     IProductRepository Product { get; }
     IProductCategoryRepository ProductCategory { get; }
     IProductImageRepository ProductImage { get; }
+    IOrderRepository Order { get; }
+    IIdempotencyKeyRepository IdempotencyKey { get; }
     Task SaveChangesAsync();
 }
