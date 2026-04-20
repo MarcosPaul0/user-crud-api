404ad94dac3efb217db20fbd00f0346d073371a7
MarcosPaul0 <marcosphip7@gmail.com>
2026-04-20 08:04:23 -0300

    feat: implements order product entity and db configuration

---
+++ src/Infrastructure/EntitiesConfiguration/OrderProductConfiguration.cs
@@ -0,0 +1,33 @@
+using AutoriaStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoriaStore.Infrastructure.EntitiesConfiguration;
+
+public class OrderProductConfiguration : IEntityTypeConfiguration<OrderProduct>
+{
+    public void Configure(EntityTypeBuilder<OrderProduct> builder)
+    {
+        builder.ToTable("order_product");
+
+        builder.HasKey(orderProduct => orderProduct.Id);
+
+        builder.Property(orderProduct => orderProduct.Id).HasColumnName("id").ValueGeneratedOnAdd();
+        builder.Property(orderProduct => orderProduct.Quantity).HasColumnName("quantity").IsRequired();
+        builder.Property(orderProduct => orderProduct.UnitPriceInCents).HasColumnName("unit_price_in_cents").IsRequired();
+        builder.Property(orderProduct => orderProduct.OrderId).HasColumnName("order_id").IsRequired();
+        builder.Property(orderProduct => orderProduct.ProductId).HasColumnName("product_id").IsRequired();
+        builder.Property(orderProduct => orderProduct.CreatedAt).HasColumnName("created_at").IsRequired().ValueGeneratedOnAdd();
+        builder.Property(orderProduct => orderProduct.UpdatedAt).HasColumnName("updated_at").IsRequired(false).ValueGeneratedOnUpdate();
+
+        builder.HasOne(orderProduct => orderProduct.Order)
+            .WithMany(order => order.ProductOrders)
+            .HasForeignKey(orderProduct => orderProduct.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(orderProduct => orderProduct.Product)
+            .WithMany()
+            .HasForeignKey(orderProduct => orderProduct.ProductId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
